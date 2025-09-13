db82730197e3d17d17ce20544cac79f6d66d31b5
mujii88 <mujtabakhan1036k@gmail.com>
2025-09-13 22:53:20 +0500

    i

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,45 +1,33 @@
-import express from "express";
-import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
-import { connectDB } from "./lib/db.js";
-import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import { app, server } from "./lib/socket.js";
-import path from "path";
-
+import express from 'express';
+import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
+import {connectDB} from './lib/db.js';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors'
+import {app,server} from './lib/socket.js'
 dotenv.config();
 
-const __dirname = path.resolve();
-const port = process.env.PORT || 5001;
 
-// Middlewares
+import path from 'path';
+const port=process.env.PORT;
+
+const __dirname=path.resolve();
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173", // dev
-      process.env.FRONTEND_URL // production
-    ],
-    credentials: true,
-  })
-);
-
-// Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes);
-
-// Serve frontend in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
+app.use(cors({
+    origin:'http://localhost:5173',
+    credentials:true
+}));
+app.use('/api/auth',authRoutes)
+app.use('/api/messages',messageRoutes);
+if (process.env.Node_ENV=='production'){
+    app.use(express.static(path.join(__dirname,"../frontend/dict")))
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist",'index.html'))
+    })
 }
-
-// Start server
-server.listen(port, () => {
-  console.log(`✅ Server is running on port ${port}`);
-  connectDB();
-});
+server.listen(port,()=>{
+    console.log(`server is running on the port ${port}`);
+    connectDB()
+}); 
